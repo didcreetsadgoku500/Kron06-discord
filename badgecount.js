08e7d8ab918f0c10c748165d98f45913aad2cc30
didcreetsadgoku500 <43486471+didcreetsadgoku500@users.noreply.github.com>
2020-01-06 13:10:03 -0500

    Rewrote badgecount.js without axios
    Need to pass errors for restricted users and getting rate limited

--- badgecount.js
+++ badgecount.js
@@ -1,32 +1,31 @@
-
- const axios = require('axios');
- //const cheerio = require('cheerio');
-
-var badgedUsers = [];
+const https = require('https');
+var HTMLParser = require('node-html-parser');
 
 function getBadgeCount(userID, callback) {
- axios('https://osu.ppy.sh/users/' + userID)
- .then(response => {
-    let html = response.data;
 
-    var badgecount = (html.match(/profile-badges/g) || []).length;
-    if (badgecount != 0) {
-     // console.log(userID + " has " + badgecount + " badges");
-      badgedUsers.push([userID, badgecount]);
-    }
-    
-    callback(userID, badgecount); 
-  })
-  }
-
-for (var i = 10505107; i <= 10505107; i++)   //okay look right now if you run this you just get rate limited for 20 minutes, put a scheduler on it LMAO
-{
-  //var j = 10505107
-  getBadgeCount(i.toString(), function (responseID, responseCount){
-      console.log("UserID " + responseID + " has " + responseCount + " badges");
-      console.log(badgedUsers);
-
-
-  })
-
+    https.get('https://osu.ppy.sh/users/' + userID, (res) => {
+        let body = "";
+        res.on('data', (data) => {body = body + data;})
+        res.on('end', () => {
+            body = HTMLParser.parse(body, {script: true});      //Parses HTML of osu profile, includes script data (important)
+            body =  body.querySelector('#json-user');           //Selects the json-user script, which holds all the important json
+            if (body == null) {
+                callback(new Error("Could not find an object of id #json-user for " + userID));
+            }
+            else {
+                body = JSON.parse(body.innerHTML);              //Parses json from the json-user script. Its like the osu api get_user query but with more details
+                callback(null, body.badges.length);                
+            }
+        })
+    });
 }
+
+//example restricted user https://osu.ppy.sh/users/10699812
+
+getBadgeCount(10699812, (err, res) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(res);
+})
