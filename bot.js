410377a7c9207a85a6c244fc9982975f43fc4a3b
didcreetsadgoku500 <43486471+didcreetsadgoku500@users.noreply.github.com>
2020-01-06 13:12:41 -0500

    Added %recent -gatari
    Only supports gatari right now, consider making it a separate module in the future

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 var discord = require('discord.js')
 var bot = new discord.Client()
+var https = require('https')
+
 
 var prefix = "%"
 
@@ -20,15 +22,53 @@ bot.on('message', msg => {
                     ;   //IF its not a number (a username)
                 } else if (isNaN(args[1])) {
 
-
+                    msg.reply("bruh");
 
                 }
                 else {
-                    msg.reply("Dawg u fucked up lmao");
+                    msg.reply("bruh");
                 }
                 break;
 
 
+
+            case "rs":
+            case "recent":              //%recent %rs
+                if  (args.includes("-gatari")) {
+                   // msg.reply("Looking for most recent gatari play for " + args[args.length - 1]);
+
+                    https.get('https://api.gatari.pw/users/get?u=' + args[args.length - 1], (response2) => {
+                        let body = '';
+                        response2.on("data", (data) => {body += data;})
+	                    response2.on("end", () => {
+                            let json = JSON.parse(body);
+                            username = json.users[0].username;
+                            uid = json.users[0].id;
+
+                            https.get('https://api.gatari.pw/user/scores/recent?id=' + uid + '&l=1&mode=0&f=1', (response) => {
+                                let body = "";
+                                response.on("data", (data) => {body += data;})
+                                response.on("end", () => {
+                                let json = JSON.parse(body);
+                                msg.channel.send(new discord.RichEmbed()
+                                    .setAuthor('Most recent gatari play for ' + username, 'https://a.gatari.pw/' + uid, 'https://osu.gatari.pw/u/' + uid)
+                                    .setTitle(json.scores[0].beatmap.song_name)
+                                    .setURL('https://osu.gatari.pw/b/' + json.scores[0].beatmap.beatmap_id)
+                                    .addField('Score:', json.scores[0].score, true)
+                                    .addField('Accuracy:', json.scores[0].accuracy.toFixed(2),true)
+                                    .addField('Rank:', ':regional_indicator_' + json.scores[0].ranking.toLowerCase() + ':', true)
+                                    .addField('Max Combo:', json.scores[0].max_combo, true)        //TODO: Show combo out of map max; will need to make api call for beatmap stats
+                                    .addField('300/100/50/Misses', json.scores[0].count_300 + '/' + json.scores[0].count_100 + '/' + json.scores[0].count_50 + '/' + json.scores[0].count_miss, true)
+                                    .addField('Performance Points:', json.scores[0].pp + "pp", true)
+                                    );
+                                })
+		                    })
+                        })
+                    
+                    })
+
+
+                }
         }
     }
   })
